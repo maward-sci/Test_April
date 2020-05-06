1479f4c4aa56ce58b73f311203c7e66d2965d465
smerolla <56319724+smerolla@users.noreply.github.com>
2020-05-06 10:36:28 -0700

    adding commits from smerolla_test to this branch (#17)
    * Update README.md
    
    * Update GitTestMapCode.R
    
    Added some text
    
    * Sarah testing Hannah's branch
    
    * Added title
    
    * Update GitTestMapCode.R
    
    * hashtag me
    
    added my name
    
    * added line 13
    
    successful merge of two branches by Sarah and Me. trying more individual commits before remerging.
    
    * adding some text
    
    * Update GitTestMapCode.R
    
    * header
    
    added metadata
    
    * final edit before merge?
    
    * closing out April
    
    good riddance April
    
    * nonsense
    
    or is it nonesense? these are the questions.
    okay nvm it's nonsense.
    
    * Carina (#5)
    
    * added line 13
    
    successful merge of two branches by Sarah and Me. trying more individual commits before remerging.
    
    * header
    
    added metadata
    
    * closing out April
    
    good riddance April
    
    * nonsense
    
    or is it nonesense? these are the questions.
    okay nvm it's nonsense.
    
    * Add files via upload
    
    * Add files via upload
    
    * Create test2
    
    * Create test3
    
    * test
    
    * Update GitTestMapCode.R
    
    * Delete Guide to Best Practices for Ocean CO2 (Dickson SOPs).pdf
    
    * Branchtest (#8)
    
    * Update GitTestMapCode.R
    
    * Delete test3
    
    * Update GitTestMapCode.R
    
    * Update GitTestMapCode.R
    
    * added blah 3
    
    * Update GitTestMapCode.R
    
    * trying to revert back to previous branch - merge reverted copy into original
    
    * Revert "trying to revert back to previous branch - merge reverted copy into original" (#16)
    
    This reverts commit 5bd68a7cd70de5262dc5b5480d812f650a98d7ea.
    
    Co-authored-by: Hannah Palmer <35040924+hannahpalmer@users.noreply.github.com>
    Co-authored-by: Melissa Ward <54690262+maward-sci@users.noreply.github.com>
    Co-authored-by: carinafish <33672027+carinafish@users.noreply.github.com>

--- GitTestMapCode.R
+++ GitTestMapCode.R
@@ -1,17 +1,44 @@
+# practicing branching & merging CF
+# last day of april! CF
+# complete nonsense idk how to spell nonesense? CF
+
 #adding code on april 2020- MW
+#the final edit
 library(ggmap)
 library(maps)
 library(mapdata)
 
+#Carina was here
+#but also here?
 states <- map_data("state")
 CA <- subset(states, region %in% c("california"))
 ggplot(data = CA) + 
   geom_polygon(aes(x = long, y = lat, group = group), fill = "gray", color = "black") + 
   coord_fixed(1.3)
 
+
+# one successful merge of two branches done!
+
+
+#Cleo was here
+
 ggplot(data = CA) + 
   geom_point(aes(x = long, y = lat))+
-  theme_classic()
+  theme_classic() +  labs(title="hello cali")
 
+
+#testing- smerolla says hello!
+
+
+##smerolla says hello again!
+##diving in the deep
+
+
+
+##a long pool noodle
+
+BLAH1
+BLAH2
 
 #adding line of code - branched off of third commit
+
